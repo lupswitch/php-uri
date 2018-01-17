fa22bf57e3a0d337f67a69da741db0febd0bf467
Nick Tsai <myintaer@gmail.com>
2018-01-17 13:33:47 +0800

    Release verion 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# php-seo-uri
-URL Standardized Re-director with Customized Setting for SEO
+*php* URI
+===============
+URL Standardized Redirector with Customized Setting for SEO
+
+[![Latest Stable Version](https://poser.pugx.org/yidas/uri/v/stable?format=flat-square)](https://packagist.org/packages/yidas/uri)
+[![Latest Unstable Version](https://poser.pugx.org/yidas/uri/v/unstable?format=flat-square)](https://packagist.org/packages/yidas/uri)
+[![License](https://poser.pugx.org/yidas/uri/license?format=flat-square)](https://packagist.org/packages/yidas/uri)
+
+
+---
+
+DEMONSTRATION
+-------------
+
+```php
+yidas\uri\Seo::trailingSlash(false)->removeIndex();
+```
+
+
+REQUIREMENTS
+------------
+
+This library requires the following:
+
+- PHP 5.4.0+
+
+---
+
+INSTALLATION
+------------
+
+Run Composer in your project:
+
+    composer require yidas/uri
+
+---
+
+COMPONENTS
+----------
+
+### Seo
+
+`yidas\uri\Seo` is a helper to handle URI for SEO, you could customized set rules in your application, then it will redirect to correct URI if the current URI is not match your expectation.
+
+The usage methods are following:
+
+#### trailingSlash()
+
+Trailing Slash Handler
+
+```
+https://www.domain.com/about/   (Switch On)
+https://www.domain.com/about    (Switch Off)
+```
+
+#### removeIndex()
+
+Most framework allows index action could be accessed by root URI of controller, this makes that way only.
+
+For example, `https://www.domain.com/about/index/` to `https://www.domain.com/about/`.
---
+++ src/Seo.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace yidas\uri;
+
+/**
+ * SEO URI Redirector
+ * 
+ * A helper to handle URI for SEO, you could customized set rules in your 
+ * application, then it will redirect to correct URI if the current URI is not 
+ * match your expectation.
+ * 
+ * @author 	Nick Tsai <myintaer@gmail.com>
+ * @since   1.0.0
+ */
+class Seo
+{
+    /**
+     * Trailing Slash Handler
+     * 
+     * @param bool Switch to keep trailing slash or not
+     * @return object self
+     * @example
+     *  https://www.domain.com/about/   (Switch On)
+     *  https://www.domain.com/about    (Switch Off)
+     */
+	public static function trailingSlash($switch=true)
+	{
+        $uri = self::getRequestUri();
+        
+        if ($switch) {
+            // Add trailing slash
+            if ($uri && strpos($uri, '?')===false 
+                && strpos($uri, '/')!==false //Except root
+                && substr($uri, -1)!=='/') {
+    
+                self::redirect($uri. '/', true);
+            }
+
+        } else {
+            // Remove trailing slash
+            if ($uri && substr($uri, -1)==='/') {
+                
+                $uri = substr($uri, 0, -1);
+    
+                self::redirect($uri, true);
+            }
+        }
+        
+        return new self;
+    }
+
+    /**
+     * Remove Index Action Name from URI
+     * 
+     * Most framework allows index action could be accessed by root URI of 
+     * controller, this makes that way only.
+     * 
+     * @param string index action string with slash prefix
+     * @return object self
+     * @example
+     *  https://www.domain.com/about/index/ to
+     *  https://www.domain.com/about/
+     */
+    public static function removeIndex($caseSensitive=false, $index='/index')
+    {
+        $uri = self::getRequestUri();
+        // Find matched position
+        $pos = $caseSensitive 
+            ? strpos($uri, $index)
+            : strpos(strtolower($uri), strtolower($index));
+
+        if ($pos && strpos($uri, '?')===false) {
+            // Get index action URI segment
+            $indexLen = strlen($index);
+            $index = substr($uri, $pos, $indexLen);
+            // Check index string is in the end of URI with trailing slash concern
+            if (strpos(substr($uri, -1-$indexLen), $index) !== false) {
+                // Replace
+                $uri = str_replace($index, '', $uri);
+                self::redirect($uri, true);
+            } 
+        }
+
+        return new self;
+    }
+
+    /**
+     * Get Server REQUEST_URI
+     * 
+     * @return string $_SERVER['REQUEST_URI']
+     */
+    public static function getRequestUri()
+    {
+        return isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : NULL;
+    }
+    
+    /**
+     * Redirect URI
+     * 
+     * @param string URL
+     * @param bool 301 Moved Permanently
+     */
+    public static function redirect($uri='/', $code301=false)
+    {
+        if ($code301) {
+
+            header("HTTP/1.1 301 Moved Permanently");
+        }
+
+        header("Location: {$uri}");
+        exit;
+    }
+}
